3a60a33a8e345e374e554dfea86e83b9860fe3fc
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-03-31 18:03:14 +0100

    - created about page

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,8 +1,33 @@
-
+import RouteHeaderBackground from "@/components/RouteHeaderBackground";
+import { aboutUsData } from "@/data";
 const page = () => {
-  return (
-    <div>About Us</div>
-  )
-}
+	return (
+		<div className="mb-20 lg:mb-[120px]">
+			<RouteHeaderBackground
+				children={
+					<div className="text-white font-bold text-2xl">Hello Church</div>
+				}
+			/>
+			<section className="container max-w-[1064px] grid gap-[35px] lg:gap-[80px] mt-10">
+				{aboutUsData.map((_, index) => {
+					const { id, title, text } = _;
+					return (
+						<div
+							className="grid gap-6 text-center"
+							key={`about-${index}.${id}`}
+						>
+							<p className="font-bold text-xl lg:text-[30px] uppercase text-[#1B1B1B]">
+								{title}
+							</p>
+							<p className="text-base lg:text-[20px] leading-8 text-start">
+								{text}
+							</p>
+						</div>
+					);
+				})}
+			</section>
+		</div>
+	);
+};
 
-export default page
+export default page;
---
+++ components/RouteHeaderBackground.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+interface Props {
+	children: ReactNode;
+}
+
+const RouteHeaderBackground = ({ children }: Props) => {
+	return (
+		<div className="h-[300px] md:h-[350px] lg:h-[470px] w-screen">
+			<div className="h-[90%] bg-[url('/about_header.png')] bg-cover bg-center flex justify-center items-center">
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default RouteHeaderBackground;
