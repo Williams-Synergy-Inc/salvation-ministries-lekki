827bfca74c63b3aa10c850a2e9e5917930ad214d
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-04-02 15:00:35 +0100

    - modified mandate section

--- components/home/Mandate.tsx
+++ components/home/Mandate.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import React from "react";
 
 const Mandate = () => {
 	return (
-		<div className="flex flex-col md:flex-row justify-center md:justify-between items-center text-center md:text-start gap-10 mb-[60px] md:mb-[100px] lg:h-[80vh]">
+		<div className="flex flex-col md:flex-row justify-center md:justify-between items-center text-center md:text-start gap-10 mb-[60px] md:mb-[100px] lg:h-[50vh]">
 			<div className="grid gap-[10px] max-w-[596px]">
-				<p className="text-[20px] md:text-[26px] lg:text-[28px] xl:text-[36px] text-primary-blue font-bold leading-[26px] md:leading-8 lg:leading-9 xl:leading-[43px]">
+				<p className="text-primary-blue font-bold text-[24px] lg:text-[30px] xl:text-[36px] leading-[26px] md:leading-9 lg:leading-10 xl:leading-[46px]">
 					A MANDATE TO ESTABLISH THE KINGDOM OF GOD ON EARTH
 				</p>
 
-				<p className="text-primary-gray text-base lg:text-xl xl:text-2xl mx-auto md:mx-0 max-w-[500px]">
-					Salvation Ministrries (Home of Success) has her global headquaters
-					currentylu at Salvation Ministries, Scintilla Center KM 22 Lekki,
-					Expressway, Lagos, Nigeria, with its fast growing branches all over
-					the world.
+				<p className="text-primary-gray text-[16px] xl:text-[18px] mx-auto md:mx-0 max-w-[500px]">
+					Salvation Ministries{" "}
+					<span className="text-primary-blue font-bold">(Home of Success)</span>{" "}
+					has her global headquaters currentylu at Salvation Ministries,
+					Scintilla Center KM 22 Lekki, Expressway, Lagos, Nigeria, with its
+					fast growing branches all over the world.
 				</p>
 			</div>
 			<div className="md:px-0">
