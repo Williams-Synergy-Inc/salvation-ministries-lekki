d9a80abb39b5978e9b0e08e4b47171b8eefeb8c8
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-04-02 18:07:06 +0100

    - made about page responsive

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,43 +1,43 @@
 import RouteHeaderBackground from "@/components/RouteHeaderBackground";
 import { aboutUsData } from "@/data";
 const page = () => {
-   const chidrenContent = (
-			<div className="flex flex-col items-center justify-center text-white gap-4 px-5 text-center translate-y-5">
-				<h1 className="font-bold text-[20px] lg:text-[46px]">
-					ABOUT THE CHURCH
-				</h1>
-				<span className="font-bold text-sm lg:text-3xl">
-					Get to know about the church, our mission, our vision etc.
-				</span>
-			</div>
-		);
+	const chidrenContent = (
+		<div className="flex flex-col items-center justify-center text-white gap-2 px-5 text-center translate-y-6">
+			<h1 className="font-bold text-[24px] md:text-[32px] lg:text-[46px]">
+				ABOUT THE CHURCH
+			</h1>
+			<span className="font-bold text-[16px] md:text-[20px] lg:text-3xl">
+				Get to know about the church, our mission, our vision etc.
+			</span>
+		</div>
+	);
 
-		return (
-			<div className="mb-20 lg:mb-[120px]">
-				<RouteHeaderBackground
-					children={chidrenContent}
-					image="/about_header.png"
-				/>
-				<section className="px-5 sm:container max-w-[1062px] grid gap-[35px] lg:gap-[80px] mt-10">
-					{aboutUsData.map((_, index) => {
-						const { id, title, text } = _;
-						return (
-							<div
-								className="grid gap-6 text-center"
-								key={`about-${index}.${id}`}
-							>
-								<p className="font-bold text-xl lg:text-[30px] uppercase text-[#1B1B1B]">
-									{title}
-								</p>
-								<p className="text-base lg:text-[20px] leading-8 text-start">
-									{text}
-								</p>
-							</div>
-						);
-					})}
-				</section>
-			</div>
-		);
+	return (
+		<div className="mb-20 lg:mb-[120px]">
+			<RouteHeaderBackground
+				children={chidrenContent}
+				image="/about_header.png"
+			/>
+			<section className="sm:container px-4 max-w-3xl lg:max-w-4xl xl:max-w-[1093px] flex flex-col gap-[35px] lg:gap-[80px] mt-10 lg:mt-20">
+				{aboutUsData.map((_, index) => {
+					const { id, title, text } = _;
+					return (
+						<div
+							className="grid gap-6 text-center"
+							key={`about-${index}.${id}`}
+						>
+							<p className="font-bold text-xl lg:text-[30px] uppercase text-[#1B1B1B]">
+								{title}
+							</p>
+							<p className="text-base lg:text-[20px] leading-8 text-start">
+								{text}
+							</p>
+						</div>
+					);
+				})}
+			</section>
+		</div>
+	);
 };
 
 export default page;
--- components/RouteHeaderBackground.tsx
+++ components/RouteHeaderBackground.tsx
@@ -5,11 +5,12 @@ interface Props {
 
 const RouteHeaderBackground = ({ children, image }: Props) => {
 	return (
-		<div className="h-[300px] md:h-[350px] lg:h-[470px] w-screen">
+		<div className="h-[250px] md:h-[300px] lg:h-[400px] w-screen">
 			<div
 				style={{ backgroundImage: `url(${image})` }}
-				className={`h-[90%] bg-cover bg-center flex justify-center items-center`}
+				className={`h-[100%] bg-cover bg-center flex justify-center items-center relative overflow-hidden`}
 			>
+				<div className="absolute inset-0 bg-gradient-to-br from-[#0155A2] to-[#003566] opacity-30 rounded-[14px]"></div>
 				{children}
 			</div>
 		</div>
